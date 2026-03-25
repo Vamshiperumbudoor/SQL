6ad912f1695f77f983d9f4de563020a292854e33
Vamshiperumbudoor <92849654+Vamshiperumbudoor@users.noreply.github.com>
2026-03-25 12:57:59 +0530

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -1,3 +1,15 @@
+
+
+
+
+
+
+
+-- I want to see the names of all employees who have a salary higher than the average salary of the whole company."
+
+-- Hint: This requires a Subquery (a query inside a query).
+-- Logic: First, find the average. Then, find the names where salary > that average.
+
 SELECT Emp_names, Salary From Office
 WHERE Salary > ( 
   SELECT AVG(Salary) 
